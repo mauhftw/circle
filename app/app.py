c5099a8c459d51485a3ee4a389a8f01588453d67
griento23 <kahnftw@gmail.com>
2017-09-06 14:24:23 -0300

    first commit

---
+++ app/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def helloworld():
+    return 'hello world!'
+
+@app.route('/hello')
+def hello():
+    return 'hello'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
